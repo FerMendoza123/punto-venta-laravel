3decbf3ba85374bda52b6c4822f6162f5eb8d493
FerMendoza123 <caifanes_10@live.com.mx>
2021-11-15 09:16:43 -0600

    Alta de producto agregada

---
+++ resources/views/visualizaProducto.blade.php
@@ -0,0 +1,23 @@
+@extends("documentoLayout")
+
+@section("contenido")
+
+<div class="container">
+    <div >
+        @if($producto->DireccionImg)
+        <img src="" alt="">
+    </div>
+
+    <div>
+        <span></span>
+        <span></span>
+        <span></span>
+
+        <a href=""></a>
+        <a href=""></a>
+    </div>
+
+</div>
+
+
+@endsection()
